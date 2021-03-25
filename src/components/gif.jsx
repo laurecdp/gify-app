ccf5904e317ba5f273269822098b4aa24773598d
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-25 16:09:24 +0100

    add solution to app

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,13 +1,21 @@
+/* eslint jsx-a11y/alt-text: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+
 import React, { Component } from 'react';
 
 class Gif extends Component {
-  render() {
-    const src = "../assets/stylesheets/logo.gif"
-    return (
-      <img src={src} alt="logo" className="gif" />
-    );
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
   }
 
+  render() {
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} className="gif" onClick={this.handleClick} />
+    );
+  }
 }
 
 export default Gif;
--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,4 @@ import '../assets/stylesheets/application.scss';
 import App from './components/app';
 
 const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(
-    <App />,
-    root
-  );
-}
+ReactDOM.render(<App />, root);
